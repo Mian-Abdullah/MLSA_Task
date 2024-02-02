24cb637dbe5791284c3b29f6305cb6ee3058138e
Abdullah Jamshaid <abdullahjamshaid844@gmail.com>
2024-02-02 16:38:50 +0500

    update

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -20,7 +20,7 @@ import PropTypes from "prop-types";
  * Need an image? Check out https://unsplash.com to download a photo you
  * freely use on your site.
  */
-import image from "../web.jpg";
+import image from "../images/web.jpg";
 
 const imageAltText = "Web and App Developer";
 
