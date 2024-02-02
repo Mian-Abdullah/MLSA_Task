ac9f770cda7b36571ac823795a25f80fb9aadf44
Abdullah Jamshaid <abdullahjamshaid844@gmail.com>
2024-02-02 16:27:19 +0500

    update

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  name: "Abdullah Jamshaid",
+  title: "Web & App Developer",
+  email: "abdullahjamshaid844@gmail.com",
+  gitHub: "Mian-Abdullah",
+  instagram: "mess_with_me2",
+  linkedIn: "abdullah-jamshaid",
   medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  twitter: "MianAbd23246412",
+  youTube: "",
 };
 
 const primaryColor = "#4E567E";
